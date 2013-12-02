526dd6e643891f3b9c708f133c0c7564455ec91d
Ramzes <ramzeska@gmail.com>
2013-12-02 23:46:05 +0400

    Initial Commit

---
+++ GumboTest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  GumboTest
+//
+//  Created by Роман Усачев on 02.12.13.
+//  Copyright (c) 2013 Роман Усачев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
