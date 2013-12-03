462fa3ebdc765c6260b2064ef9c38e95e3b78780
Ramzes <ramzeska@gmail.com>
2013-12-03 08:42:51 +0400

    Selectors

--- GumboTest/main.c
+++ GumboTest/main.c
@@ -20,6 +20,26 @@ struct SelectorFind {
     struct SelectorFind* parent;
 };
 
+
+bool hasClass(const char *classDefinition,const char *className) {
+    char *match=0;
+    unsigned long end = (int)classDefinition + strlen(classDefinition);
+    
+    if (strcmp(classDefinition,className)==0)
+        return true;
+    
+    while ( (int)match < end) {
+        match = strstr(classDefinition,className);
+        if (match == NULL)
+            return false;
+        if ( (match == classDefinition || match[-1] == ' ') && (match[strlen(className)] == ' ' || match[strlen(className)] == 0))
+            return true;
+        
+        classDefinition = &match[strlen(className)];
+    }
+    return false;
+}
+
 // head
 // body
 // body h1
@@ -27,10 +47,8 @@ struct SelectorFind {
 // body nav ul
 // body nav ul li
 // body nav ul li a
-bool testSelecorAgainst(struct SelectorFind *sel,GumboNode *node) {
-    while (sel->next != 0) {
-        sel = sel->next;
-    }
+bool testSelecorAgainst(const struct SelectorFind *sel,GumboNode *node) {
+    
     // ff => to the end of selector list
     bool match = false;
     
@@ -39,38 +57,40 @@ bool testSelecorAgainst(struct SelectorFind *sel,GumboNode *node) {
         GumboAttribute *class = gumbo_get_attribute(&node->v.element.attributes, "class");
         GumboAttribute *id = gumbo_get_attribute(&node->v.element.attributes, "id");
         
+        match = true;
         if (sel->tag != 0)
             match = node->v.element.tag == sel->tag;
-        if (strlen(sel->class)!=0) {
-            if (class)
-                match = strcmp(class->value,sel->class) == 0;
-            else
+        
+        if (sel->class != NULL) {
+            if (class) {
+                match &= hasClass(class->value,sel->class);
+            } else
                 match = false;
         }
         
-        if (strlen(sel->id)!=0) {
+        if (sel->id != NULL) {
             if (id)
-                match = strcmp(id->value,sel->id) == 0;
+                match &= strcmp(id->value,sel->id) == 0;
             else
                 match = false;
         }
         
         // <==
         if (!match) {
-            if (sel->next == 0)// если первый селектор справа не подошел - забиваем
+            if (sel->next == NULL)// если первый селектор справа не подошел - забиваем
                 return false;
             else if (sel->directDescendant) {
                 return false; // требование прямого потомка не выполнено - забиваем
-            } else if (node->parent != 0) { // ну теперь можно попробовать уровнем выше с тем же селектором, вдруг повезет?
+            } else if (node->parent != NULL) { // ну теперь можно попробовать уровнем выше с тем же селектором, вдруг повезет?
                 node = node->parent;
             } else {
                 return false;
             }
         } else { // совпадение
-            if (sel->parent == 0) { // селекторы кончились
+            if (sel->parent == NULL) { // селекторы кончились
                 return true;
             }
-            if (node->parent == 0) { // кончились предки, а селекторы нет, облом
+            if (node->parent == NULL) { // кончились предки, а селекторы нет, облом
                 return false;
             }
             sel = sel->parent;
@@ -82,10 +102,27 @@ bool testSelecorAgainst(struct SelectorFind *sel,GumboNode *node) {
 }
 
 void printNode(GumboNode *node) {
-    printf("Yay! <%s>\n",gumbo_normalized_tagname(node->v.element.tag));
+    if (node->type == GUMBO_NODE_TEXT) {
+        printf("{ \"text\": \"%s\" }\n",node->v.text.text);
+    } else if (node->type == GUMBO_NODE_ELEMENT) {
+        printf("\t{ \"tag\": \"%s\", \"params\": [ ",gumbo_normalized_tagname(node->v.element.tag));
+        for (int i =0; i < node->v.element.attributes.length; i++ ) {
+            GumboAttribute *a = node->v.element.attributes.data[i];
+            if (i>0)
+                printf(",\n");
+            printf(" \"%s\": \"%s\"",a->name,a->value);
+        }
+        printf("], \"children\": [\n");
+        for (int i =0; i < node->v.element.children.length; i++ ) {
+            if (i>0)
+                printf(",\n");
+            printNode(node->v.element.children.data[i]);
+        }
+        printf("]}\n");
+    }
 }
 
-void findObjects(GumboNode *rootNode, struct SelectorFind *sel) {
+void findObjects(GumboNode *rootNode, const struct SelectorFind *sel) {
     const GumboVector* root_children = &rootNode->v.element.children;
     
     for (int i=0;i < root_children->length;i++) {
@@ -99,14 +136,12 @@ void findObjects(GumboNode *rootNode, struct SelectorFind *sel) {
             }
             
             // TestForSelector
-            if (testSelecorAgainst(sel, child)) {
+            if (testSelecorAgainst(sel, child)==true ) {
                 printNode(child);
             }
         }
         
-        if (child->type == GUMBO_NODE_TEXT) {
-            printf("Title: %s\n",child->v.text.text);
-        }
+        
         
     }
 }
@@ -117,13 +152,7 @@ void findObjects(GumboNode *rootNode, struct SelectorFind *sel) {
  **/
 struct SelectorFind* newFindSelector(unsigned long strMaxSize) {
     struct SelectorFind* sel;
-    sel = malloc(sizeof(struct SelectorFind));
-    sel->tag = 0;
-    sel->directDescendant = false;
-    sel->id = malloc(strMaxSize);
-    sel->class = malloc(strMaxSize);
-    sel->next = 0;
-    sel->parent = 0;
+    sel = calloc(sizeof(struct SelectorFind),sizeof(struct SelectorFind));
     return sel;
 }
 
@@ -131,45 +160,52 @@ struct SelectorFind* newFindSelector(unsigned long strMaxSize) {
  * парсит селекторы
  **/
 struct SelectorFind* parseSelector(char* selector) {
-    char* buf;
+    char *buf,*nameBuf;
     struct SelectorFind *sel, *first;
     bool newTag = false;
     
     sel = newFindSelector(strlen(selector));
     first = sel;
     
-    buf = malloc(strlen(selector));
+    buf = calloc(strlen(selector),1);
+    nameBuf = buf;
+    
+
     
     for (int i=0; i < strlen(selector); i++ ) {
         if (selector[i] == '.') {
-            if (strlen(buf) > 0) {
-                sel->tag = gumbo_tag_enum(buf);
+            if (strlen(nameBuf) > 0) {
+                sel->tag = gumbo_tag_enum(nameBuf);
             }
-            buf = sel->class;
+            buf = sel->class = calloc(strlen(selector),1);
             continue;
         } else if (selector[i] == '#') {
             if (strlen(buf) > 0) {
                 sel->tag = gumbo_tag_enum(buf);
             }
-            buf = sel->id;
+            buf = sel->id = calloc(strlen(selector),1);
             continue;
         } else if (selector[i] == ' ') {
-            if (strlen(buf) > 0) {
+            if (strlen(nameBuf) > 0) {
                 sel->tag = gumbo_tag_enum(buf);
+                buf = calloc(strlen(selector),1);
+                nameBuf = buf;
                 newTag = true;
             }
-           
             continue;
         } else if (selector[i] == '>') {
-            if (strlen(buf) > 0) {
+            if (strlen(nameBuf) > 0) {
                 sel->tag = gumbo_tag_enum(buf);
+                buf = calloc(strlen(selector),1);
+                nameBuf = buf;
+                newTag = true;
+                sel->directDescendant = true;
             }
-            newTag = true;
-            sel->directDescendant = true;
             continue;
         } else {
             if (newTag) {
-                buf = malloc(strlen(selector));
+                buf = calloc(strlen(selector),1);
+                nameBuf = buf;
                 sel->next = newFindSelector(strlen(selector));
                 sel->next->parent = sel;
                 sel = sel->next;
@@ -180,44 +216,60 @@ struct SelectorFind* parseSelector(char* selector) {
             buf[l+1] = 0;
         }
     }
+    if (strlen(nameBuf) > 0 && sel->tag == 0) {
+        sel->tag = gumbo_tag_enum(nameBuf);
+    }
+    if (sel->tag == GUMBO_TAG_UNKNOWN && sel->id == NULL && sel->class == NULL) {
+        sel = sel->parent;
+        free(sel->next);
+        sel->next = NULL;
+    }
     return first;
 }
 
 int main(int argc, const char * argv[])
 {
 
-    
+    int selSize=0;
     char *selector;
     
     if ( argc > 1 ) {
         for (int i = 1 ; i < argc; i++) {
-            selector = realloc(selector,strlen(argv[i]));
+            selSize += strlen(argv[i])+1;
+            selector = realloc(selector,selSize+1);
             strcat(selector, " ");
             strcat(selector, argv[i]);
         }
         printf("Text selector: %s\n",selector);
     } else {
-        printf("Usage: cat htmlfile | %s <selector>\n\tWhere <selector> - ex. body div#div-id a.className > span",argv[0]);
+        printf("Usage: cat htmlfile | %s <selector>\n\tWhere <selector> - ex. body div#div-id a.className > span\n",argv[0]);
         return 1;
     }
     
-    char buf[1024];
-    char *page;
+    char *buf = malloc(1024);
+    char *page=calloc(1024,1);
     unsigned long readBytes=0, sz = 0;
-   // FILE *f = fopen(stdin, "r");
+    FILE *f = fopen("/Users/ramzes/Documents/GumboTest/GumboTest/build/Release/test.html", "r");
     
-    while (!feof(stdin) > 0) {
-        readBytes = fread(buf, 1, 1024, stdin);
+    while (!feof(f) > 0) {
+        readBytes = fread(buf, 1, 1024, f);
         sz += readBytes;
         //sz = fread(buf, 1, 1024, f);
-        page = realloc(page,sz);
+        page = realloc(page,sz+1);
         strncat(page,buf,readBytes);
     }
-    //fclose(stdin);
+    printf("Parsing %lu bytes\n",sz);
+    fclose(f);
     
     GumboOutput* output = gumbo_parse(page);
     
     struct SelectorFind *pSel = parseSelector(selector);
+    
+    printf("=> %s#%s.%s\n", gumbo_normalized_tagname(pSel->tag),pSel->id,pSel->class);
+    while (pSel->next != 0) {
+        pSel = pSel->next;
+        printf("=> %s#%s.%s\n",gumbo_normalized_tagname(pSel->tag),pSel->id,pSel->class);
+    }
     
     findObjects(output->root,pSel);
     
